adb19e8b018d70ba9dcf9e9a4463d4167360ab42
RICARDO RIBEIRO DE MACEDO FILHO <rickhard@bol.com.br>
2022-08-29 18:58:44 -0300

    Update app.js

--- assets/js/data/app.js
+++ assets/js/data/app.js
@@ -5,12 +5,12 @@
 
   // Your web app's Firebase configuration
   const firebaseConfig  = {
-    apiKey: "AIzaSyCPvNIS1dJolQjBJzwGrlIAi9fE0HuwTwc",
-    authDomain: "mundoinvertidodio.firebaseapp.com",
-    projectId: "mundoinvertidodio",
-    storageBucket: "mundoinvertidodio.appspot.com",
-    messagingSenderId: "984688227950",
-    appId: "1:984688227950:web:a8d512fa648a458536187f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // Initialize Firebase
